cae97169c54e859c13a66dcff553829191ea7d80
Your Name <you@example.com>
2023-09-02 15:17:17 +0600

    cros plm

--- database/mongooseDb.js
+++ database/mongooseDb.js
@@ -5,7 +5,7 @@ const { default: mongoose } = require("mongoose")
 exports.mongooseDb_connect = async () => {
 
     try {
-        const { connection } = mongoose.connect(process.env.database_url, { dbName: 'dshop-ecommerce-web' })
+        const { connection } = await mongoose.connect(process.env.database_url, { dbName: 'dshop-ecommerce-web' })
         console.log(`✅ mongoose connect at :`, connection.host)
     } catch (error) {
         console.log('⛔ datebase connect failed.')
